369f0e1a1e20011c37fec0b095402315affa8dd3
Nelson Mathurent <nelmathu@yahoo.com>
2019-09-05 21:51:28 -0700

    Adding the files for the first time

---
+++ StringCalculator/Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,32 @@
+﻿// Unit Testing module for String Calculator Program
+// Created by Nelson Mathurent
+// Date: Sep 05 2019
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+
+namespace Calculator.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void AddNumbers_NoParameters_ReturnZero()
+        {
+            //Arrange 
+            int expectedValue = 0;
+            string numbersToTest = "";
+
+            // Act 
+            int actualValue = StringCalculator.Calculator.AddNumbers(numbersToTest);
+
+            // Assert
+
+            Assert.AreEqual(expectedValue, actualValue);
+
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿// String Calculator Program
+// Created by Nelson Mathurent
+// Date: Sep 05 2019
+
+using System;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public static int AddNumbers(string numbers)
+        {
+            return string.IsNullOrEmpty(numbers) ? 0 : int.Parse(numbers);
+        }
+    }
+}
